d12dc55f23ddb9f7e6adfb3fe9471afff3895802
Rafiester <rafi.haqqi@gmail.com>
2021-03-05 18:45:57 +0000

    MiuiCamera: Cleanup
    * Decommonize only for mido
    
    Signed-off-by: Rafiester <rafi.haqqi@gmail.com>

--- config.mk
+++ config.mk
@@ -46,6 +46,7 @@ PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/system/lib64/libmorpho_memory_allocator.so:system/lib64/libmorpho_memory_allocator.so \
     $(VENDOR_PATH)/system/lib64/libmorpho_panorama.so:system/lib64/libmorpho_panorama.so \
     $(VENDOR_PATH)/system/lib64/libmorpho_panorama_gp.so:system/lib64/libmorpho_panorama_gp.so \
+    $(VENDOR_PATH)/system/etc/device_features/mido.xml:system/etc/device_features/mido.xml \
 
 PRODUCT_PACKAGES += \
     MiuiCamera
